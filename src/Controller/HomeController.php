7b9295a282126d3a6f50fdc7e27f10cdb06e212c
Valentin <valentin.chaillon@gmail.com>
2019-06-14 17:07:26 +0200

    add css and scass + squeltte home

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,16 +2,28 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
+use App\Form\ArticleType;
+use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/")
+ */
 class HomeController extends AbstractController
 {
     /**
-     * @Route("/", name="home")
+     * @Route("/", name="article_index", methods={"GET"})
      */
-    public function index()
+    public function index(): Response
     {
-        return $this->render('static/home.html.twig');
+        return $this->render('static/home.html.twig', [
+            'hhey' => 'hey',
+        ]);
     }
+
+
 }
